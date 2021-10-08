380e9b9e9453f90342578b2cc4ef7468134b3f17
calexandar <alexandercvet243@gmail.com>
2021-10-08 12:12:17 +0200

    app_completed

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>Version 1.0.0</h3>
+  <router-link to="/" >Go Back</router-link>
+</template>
+
+<script>
+export default {
+    name: "Abou",
+    props: {
+        showAddTask: Boolean,
+    },
+}
+</script>
+
+<style>
+
+</style>
